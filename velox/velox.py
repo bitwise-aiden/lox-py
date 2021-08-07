85ab9db04cd8d03ca92ac1b0ed408a80dc56573e
Aiden Storey <git@storey.dev>
2021-08-07 14:21:40 -0400

    Rearrange files

--- velox/velox.py
+++ velox/velox.py
@@ -40,3 +40,15 @@ class Velox:
 
         for token in tokens:
             print(token)
+
+
+if __name__ == '__main__':
+    _, *args = sys.argv
+
+    if len(args) > 1:
+        print('Usage: velox [script]')
+        sys.exit(64)
+    elif len(args) == 1:
+        Velox.run_file(args[0])
+    else:
+        Velox.run_prompt()
